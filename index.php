898efc8e308e145d308704ac2aaf7991d7b5e70d
vivincl <viviannicolev@gmail.com>
2026-09-14 21:24:39 -0300

    Cria página inicial do projeto

---
+++ index.php
@@ -0,0 +1,11 @@
+<!DOCTYPE html>
+<html lang="pt-BR">
+<head>
+ <meta charset="UTF-8">
+ <title>Projeto Git</title>
+</head>
+<body>
+ <h1>Projeto da dupla</h1>
+ <p>Projeto criado para praticar Git e GitHub.</p>
+</body>
+</html>
